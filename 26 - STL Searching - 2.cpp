491ab1faebc1092eda2a37b8a34fad3d21add2f9
yd3-test4 <114435585+yd3-test4@users.noreply.github.com>
2022-10-26 14:12:56 +0530

    STL Searching Program

---
+++ 26 - STL Searching - 2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int find_largest_num_smaller_than(vector<int> &v, int search) {
+   vector<int>::iterator  =  
+}
+
+int main() {
+    vector<int> v {1, 4, 4, 4, 4, 9, 9, 10, 11};
+    for(auto &i: v) cout << i << ", "; cout << " | Size : " << v.size() << endl;
+    
+    sort(v.begin(), v.end());
+    for(auto &i: v) cout << i << ", "; cout << " | Size : " << v.size() << endl;
+
+
+}
